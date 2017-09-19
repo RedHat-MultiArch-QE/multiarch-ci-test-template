854e33c6ee71f80ff509dedd326b8d6d1145aace
Jason DeTiberus <jdetiber@redhat.com>
2017-09-19 14:22:16 -0400

    attempt to define WithDuffyHost step

---
+++ vars/withDuffyHost.groovy
@@ -0,0 +1,16 @@
+import org.centos.Utils
+
+def call(Closure body) {
+  def utils = New Utils()
+  try {
+    utils.allocateDuffyCciskel('test')
+    body()
+  }
+  catch (err) {
+    echo err.getMessage()
+    throw err
+  }
+  finally {
+    utils.teardownDuffyCciskel('test')
+  }
+}
