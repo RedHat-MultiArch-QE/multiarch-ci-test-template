2d643884b2e943eda5fdf5e9b8b02dc24ee023d2
Jason DeTiberus <jdetiber@redhat.com>
2017-09-19 15:07:41 -0400

    attempt to define WithDuffyHost step

--- vars/withDuffyHost.groovy
+++ vars/withDuffyHost.groovy
@@ -1,6 +1,6 @@
 def call(Closure body) {
 
-  def utils = library('cico-pipeline-library').org.centos.Utils.new(this)
+  def utils = library('cico-pipeline-library@master').org.centos.Utils.new(this)
   try {
 //    utils.allocateDuffyCciskel('test')
     body()
