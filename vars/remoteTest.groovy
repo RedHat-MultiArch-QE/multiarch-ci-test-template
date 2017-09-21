e375312d1f1e78376882beacd35afd17c5902cd5
Jason DeTiberus <jdetiber@redhat.com>
2017-09-21 11:38:51 -0400

    fix quoting

--- vars/remoteTest.groovy
+++ vars/remoteTest.groovy
@@ -2,7 +2,7 @@ def call(List<String> remote_tasks=[]) {
   try {
     def test_hostname = readFile('duffy.hostname')
     echo "test_hostname: ${test_hostname}"
-    withEnv(['test_hostname=${test_hostname}']) {
+    withEnv(["test_hostname=${test_hostname}"]) {
       sh('''#!/usr/bin/bash -xeu
             ssh -o StrictHostKeyChecking=no ${test_hostname} hostname
          ''')
