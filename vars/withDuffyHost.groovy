f47f5348459e529df46cc9a0493c2b618939862c
Jason DeTiberus <jdetiber@redhat.com>
2017-09-21 11:32:25 -0400

    fix quoting of string

--- vars/withDuffyHost.groovy
+++ vars/withDuffyHost.groovy
@@ -20,7 +20,7 @@ def call(Closure body) {
     archiveArtifacts 'duffy.ssid'
     archiveArtifacts 'duffy.hostname'
     archiveArtifacts 'duffy.inventory'
-    def test_hostname=readFile(duffy.hostname)
+    def test_hostname=readFile('duffy.hostname')
     echo "test_hostname: ${duffy_hostname}"
     body()
   }
