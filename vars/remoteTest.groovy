044ffe1e0c05be85517029c9a587a4e254bada57
Jason DeTiberus <jdetiber@redhat.com>
2017-09-21 11:37:33 -0400

    make test_hostname available in env

--- vars/remoteTest.groovy
+++ vars/remoteTest.groovy
@@ -2,10 +2,11 @@ def call(List<String> remote_tasks=[]) {
   try {
     def test_hostname = readFile('duffy.hostname')
     echo "test_hostname: ${test_hostname}"
-    sh('''#!/usr/bin/bash -xeu
-          ssh -o StrictHostKeyChecking=no ${test_hostname} hostname
-       ''')
-
+    withEnv(['test_hostname=${test_hostname}']) {
+      sh('''#!/usr/bin/bash -xeu
+            ssh -o StrictHostKeyChecking=no ${test_hostname} hostname
+         ''')
+    }
   }
   catch (err) {
     echo err.getMessage()
