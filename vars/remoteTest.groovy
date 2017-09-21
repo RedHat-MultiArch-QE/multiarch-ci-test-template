d78c5a2d7f877e5ed8b2be3707ff1b7ff65ec470
Jason DeTiberus <jdetiber@redhat.com>
2017-09-21 11:50:44 -0400

    fix

--- vars/remoteTest.groovy
+++ vars/remoteTest.groovy
@@ -4,7 +4,8 @@ def call(List<String> remote_tasks=[]) {
     echo "test_hostname: ${test_hostname}"
     withEnv(["test_hostname=${test_hostname}"]) {
       sh('''#!/usr/bin/bash -xeu
-            ssh -o StrictHostKeyChecking=no ${test_hostname} hostname
+            ssh_opts="-t -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
+            ssh ${ssh_opts} ${test_hostname} hostname
          ''')
     }
   }
