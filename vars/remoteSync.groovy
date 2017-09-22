fb9eb20ffc8ed4283b30fda6b43b15550863d7cd
Jason DeTiberus <jdetiber@redhat.com>
2017-09-22 16:32:37 -0400

    remove whitespace from hostname

--- vars/remoteSync.groovy
+++ vars/remoteSync.groovy
@@ -1,7 +1,7 @@
 def call(List<String> dirs=[]) {
   try {
     unstash('test.hostname')
-    def test_hostname = readFile('test.hostname')
+    def test_hostname = readFile('test.hostname').replaceAll("\\s","")
     echo "test_hostname: ${test_hostname}"
     for (d in dirs) {
       withEnv(["test_hostname=${test_hostname}", "d=${d}"]) {
