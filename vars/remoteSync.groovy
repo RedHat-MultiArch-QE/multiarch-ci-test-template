985b97dc5602b2e0d08a60e134e39301c6a92cb2
Jason DeTiberus <jdetiber@redhat.com>
2017-09-22 16:03:04 -0400

    attempt to sync origin from slave

---
+++ vars/remoteSync.groovy
@@ -0,0 +1,19 @@
+def call(List<String> dirs=[]) {
+  try {
+    unstash('test.hostname')
+    def test_hostname = readFile('test.hostname')
+    echo "test_hostname: ${test_hostname}"
+    for (d in dirs) {
+      withEnv(["test_hostname=${test_hostname}", "d=${d}"]) {
+        sh('''#!/usr/bin/bash -xeu
+              ssh_opts="-t -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -l root"
+	      rsync -avzhe "ssh ${ssh_opts}" "${test_hostname}:${d}" "${d}"
+           ''')
+      }
+    }
+  }
+  catch (err) {
+    echo err.getMessage()
+    throw err
+  }
+}
