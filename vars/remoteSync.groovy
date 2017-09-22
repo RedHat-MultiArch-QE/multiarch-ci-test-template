40e8385fa48f1731f37db7966ec73ea204ea8408
Jason DeTiberus <jdetiber@redhat.com>
2017-09-22 16:41:14 -0400

    no verbose on rsync

--- vars/remoteSync.groovy
+++ vars/remoteSync.groovy
@@ -7,7 +7,7 @@ def call(List<String> dirs=[]) {
       withEnv(["test_hostname=${test_hostname}", "d=${d}"]) {
         sh('''#!/usr/bin/bash -xeu
               ssh_opts="-o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -l root"
-	      rsync -avzhe "ssh ${ssh_opts}" "${d}" "${test_hostname}:${d}"
+	      rsync -azhe "ssh ${ssh_opts}" "${d}" "${test_hostname}:${d}"
            ''')
       }
     }
