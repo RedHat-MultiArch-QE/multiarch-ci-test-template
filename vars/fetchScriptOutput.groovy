ad962f5c3d6573155311cbd78fa624a17cddd181
Jason DeTiberus <jdetiber@redhat.com>
2017-09-27 12:17:52 -0400

    fix fetchScriptOutput

--- vars/fetchScriptOutput.groovy
+++ vars/fetchScriptOutput.groovy
@@ -3,7 +3,7 @@ def call() {
     unstash('test.hostname')
     def test_hostname = readFile('test.hostname').replaceAll("\\s","")
     echo "test_hostname: ${test_hostname}"
-    withEnv(["test_hostname=${test_hostname}", "d=${d}"]) {
+    withEnv(["test_hostname=${test_hostname}"]) {
       sh('''#!/usr/bin/bash -xeu
             ssh_opts="-o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -l root"
             rsync -azhe "ssh ${ssh_opts}" "${test_hostname}:origin/_output" "origin"
