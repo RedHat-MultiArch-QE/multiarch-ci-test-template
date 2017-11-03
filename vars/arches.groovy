788f74c3a2d67cff1625c5b49b1a18572e0456e2
Jeremy Poulin <jpoulin@redhat.com>
2017-11-03 14:11:55 -0400

    Update arches.groovy

--- vars/arches.groovy
+++ vars/arches.groovy
@@ -1,12 +1,8 @@
-def Closure printVal = { a ->
-  println a
-}
-
 def call(List<String> arches = [], Closure body) { 
   // Loop through the submitted arches and wrap each body call in a closure
   def archTasks = [:]
   for (arch in arches) {
-    printVal(arch)
+    body(arch)
     //archTasks[arch] = wrapFunction(arch, printVal)
   }
   
