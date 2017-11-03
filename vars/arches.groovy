a1265d6c1c026e0eb3ffa5bb44187f1c28e33401
Jeremy Poulin <jpoulin@redhat.com>
2017-11-03 14:09:14 -0400

    Update arches.groovy

--- vars/arches.groovy
+++ vars/arches.groovy
@@ -6,13 +6,9 @@ def call(List<String> arches = [], Closure body) {
   // Loop through the submitted arches and wrap each body call in a closure
   def archTasks = [:]
   for (arch in arches) {
-    archTasks[arch] = wrapFunction(arch, printVal)
+    printVal(arch)
+    //archTasks[arch] = wrapFunction(arch, printVal)
   }
   
-  parallel archTasks
-}
-
-def wrapFunction(String arch, Closure body) {
-  def a = new String(arch)
-  return { body(a) }
+  //parallel archTasks
 }
