79b9de4367bd2c495bd7098b1c9e661c28af386d
Jeremy Poulin <jpoulin@redhat.com>
2017-11-03 12:23:27 -0400

    Update arches.groovy

--- vars/arches.groovy
+++ vars/arches.groovy
@@ -1,7 +1,14 @@
 def call(List<String> arches = [], Closure body) {
+  // This closure is necessary to ensure that the arch string param gets wrapped immutably
+  def Closure wrapBody = { arch ->
+    def a = new String(arch)
+    return { body(a) }
+  }
+  
+  // Loop through the submitted arches and wrap each body call in a closure
   def archTasks = [:]
   for (arch in arches) {
-    archTasks[arch] = { body(new String(arch)) }
+    archTasks[arch] = wrapBody(arch)
   }
 
   parallel archTasks
