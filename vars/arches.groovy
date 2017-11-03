a6a1fa871b38422dc28e09c02c85b84c96592012
Jeremy Poulin <jpoulin@redhat.com>
2017-11-03 11:22:21 -0400

    Added function call for new scope

--- vars/arches.groovy
+++ vars/arches.groovy
@@ -1,8 +1,13 @@
 def call(List<String> arches = [], Closure body) {
+  def wrapBody(arch) {
+    return { body(arch) }
+  }
+  
   println arches
   archTasks = arches.collectEntries {
-    [ (it) : { body(it) } ]
+    [ (it) : wrapBody(it) ]
   }
+  
   println archTasks
   parallel archTasks
 }
