aa244fe0f5d022717f2916607a76d787fec4f841
Jeremy Poulin <jpoulin@redhat.com>
2017-11-03 12:15:04 -0400

    Simplifying arches step now that the string param is actually behaving correctly.

--- vars/arches.groovy
+++ vars/arches.groovy
@@ -1,15 +1,8 @@
 def call(List<String> arches = [], Closure body) {
-  def Closure wrapBody = { String a, Closure b ->
-    println "String ${a} is totally immutable, like for sure - now wrapping it in a closure"
-    def s = new String(a)
-    return { b(s) }
-  }
-  
-  println arches
   def archTasks = [:]
   for (arch in arches) {
     archTasks[arch] = { body(new String(arch)) }
   }
-  
+
   parallel archTasks
 }
