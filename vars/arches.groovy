95fe508b216e8526d53aa4b95c3e4ef9b05ed0ca
Jeremy Poulin <jpoulin@redhat.com>
2017-11-03 11:31:06 -0400

    Switched wrapper to closure

--- vars/arches.groovy
+++ vars/arches.groovy
@@ -1,11 +1,11 @@
 def call(List<String> arches = [], Closure body) {
-  def wrapBody(arch) {
-    return { body(arch) }
+  def Closure wrapBody = { (String a, Closure b) ->
+    return { b(a) }
   }
   
   println arches
   archTasks = arches.collectEntries {
-    [ (it) : wrapBody(it) ]
+    [ (it) : wrapBody(it, body) ]
   }
   
   println archTasks
