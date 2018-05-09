3b014155554c05f2a3380d09c72683c6c5453776
Jeremy Poulin <jpoulin@redhat.com>
2018-05-09 13:20:07 -0400

    Attempting to add rhpkg repo for ci

--- tests/scripts/rhel-system-roles/test.sh
+++ tests/scripts/rhel-system-roles/test.sh
@@ -3,9 +3,15 @@
 # Attempts to clone down the test package and run it
 workdir=$(pwd)
 
-sh-keyscan -H 10.19.208.80 >> ~/.ssh/known_hosts
+touch ~/.ssh/known_hosts
+ssh-keyscan -H 10.19.208.80 >> ~/.ssh/known_hosts
 ssh-keyscan -H pkgs.devel.redhat.com >> ~/.ssh/known_hosts
-sudo yum install rhpkg
+
+sudo yum install -y yum-utils
+wget -O pnt-devops-rhpkg-continuous-delivery.repo https://copr.devel.redhat.com/coprs/pnt-devops/rhpkg-continuous-delivery/repo/rhel-7/pnt-devops-rhpkg-continuous-delivery
+sudo yum-config-manager --add-repo pnt-devops-rhpkg-continuous-delivery.repo
+sudo yum install -y rhpkg
+
 rhpkg clone tests/rhel-system-roles
 cd rhel-system-roles/Sanity/Upstream-testsuite
 make run > $workdir/artifacts/output.txt
