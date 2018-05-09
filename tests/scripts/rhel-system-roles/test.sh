83651a081306b6a6a6164dd29b6d4d628321b543
Jeremy Poulin <jpoulin@redhat.com>
2018-05-09 11:47:32 -0400

    Now doing keyscan and installing via rhpkg

--- tests/scripts/rhel-system-roles/test.sh
+++ tests/scripts/rhel-system-roles/test.sh
@@ -2,6 +2,10 @@
 #
 # Attempts to clone down the test package and run it
 workdir=$(pwd)
-git clone ssh://jpoulin@pkgs.devel.redhat.com/tests/rhel-system-roles
+
+sh-keyscan -H 10.19.208.80 >> ~/.ssh/known_hosts
+ssh-keyscan -H pkgs.devel.redhat.com >> ~/.ssh/known_hosts
+sudo yum install rhpkg
+rhpkg clone tests/rhel-system-roles
 cd rhel-system-roles/Sanity/Upstream-testsuite
 make run > $workdir/artifacts/output.txt
