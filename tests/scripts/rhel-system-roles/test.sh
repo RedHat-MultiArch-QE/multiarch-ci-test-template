f9c34c671f3de10b0693479ae950219e18d3d948
Jeremy Poulin <jpoulin@redhat.com>
2018-05-09 15:08:46 -0400

    Updated tests to get rhpkg from dev dist

--- tests/scripts/rhel-system-roles/test.sh
+++ tests/scripts/rhel-system-roles/test.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #
 # Attempts to clone down the test package and run it
+cd "$(dirname ${BASH_SOURCE[0]})"
 workdir=$(pwd)
 
 touch ~/.ssh/known_hosts
@@ -8,10 +9,11 @@ ssh-keyscan -H 10.19.208.80 >> ~/.ssh/known_hosts
 ssh-keyscan -H pkgs.devel.redhat.com >> ~/.ssh/known_hosts
 
 sudo yum install -y yum-utils
-wget -O pnt-devops-rhpkg-continuous-delivery.repo https://copr.devel.redhat.com/coprs/pnt-devops/rhpkg-continuous-delivery/repo/rhel-7/pnt-devops-rhpkg-continuous-delivery
-sudo yum-config-manager --add-repo pnt-devops-rhpkg-continuous-delivery.repo
+curl -L -O http://download.devel.redhat.com/rel-eng/internal/rcm-tools-rhel-7-server.repo
+sudo yum-config-manager --add-repo rcm-tools-rhel-7-server.repo
 sudo yum install -y rhpkg
 
 rhpkg clone tests/rhel-system-roles
 cd rhel-system-roles/Sanity/Upstream-testsuite
-make run > $workdir/artifacts/output.txt
+mkdir -p $workdir/artifacts
+make &> $workdir/artifacts/output.txt run
